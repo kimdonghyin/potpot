66ab05c3af67148b3691fcb08df16d2c2cbf659d
sar1ce <kimdong7596@naver.com>
2021-05-03 18:05:11 +0900

    Add files via upload

--- json_parsing.py
+++ json_parsing.py
@@ -12,7 +12,7 @@ import binascii
 from pymongo import MongoClient
 from pymongo.cursor import CursorType
 
-ip_list = [] #this is one of ip list
+ip_list = [] # this is one of ip list
 total_ip = [] # ip 전부
 
 def sort_ip(attack_ip): # compare ip
@@ -28,6 +28,14 @@ def count_ip(): # count ip
         countIP_dic[i] = str(total_ip.count(i))
 
     return countIP_dic
+
+def json_key_present(json, key): #json key checking
+    try:
+        check = json[key]
+    except KeyError:
+        return False
+
+    return True
 
 def cCode(date): # take countryCode
     
@@ -70,9 +78,10 @@ def insert_DB(total_dic):
     host = "localhost"
     port = 27017
     mongo = MongoClient(host, port)
-    db_name = 'potpot_DB'
+    db_name = 'potpot'
     collection_name = 'potpot'
 
+    print("DB Connect")
     #a = list(total_dic.keys())
     #b = list(total_dic.values())
 
@@ -84,19 +93,9 @@ def insert_DB(total_dic):
 
     p_hash = re.compile('Hash : \w+')
 
-    p_command = re.compile('Command : << $& >>')
-    #temp = b[0]
-    #temp_comm = temp[p_command.search(b[0]).end():]
-    #print(p_command.search(b[0]).start(), temp[p_command.search(b[0]).end():])
+    p_command = re.compile("'\/?[A-Za-z0-9_ | \/\->\',]*")
 
-    # Country Code : \w\w
-    # Connection Count : \d+
-    # Command : << $& >>
-
-    # for i, j in total_dic.items():
-    #     print("----test---")
-    #     print(type(p_hash.search(j).group()), p_hash.search(j).group())
-
+    print(total_dic.values())
 
     for i, j in total_dic.items():
         h = ''
@@ -106,17 +105,20 @@ def insert_DB(total_dic):
             h = 'none'
         else:
             h = str(p_hash.search(j).group())
+            #print(h)
 
         if p_command.search(j) == None:
             c = 'none'
+
         else:
             c = str(p_command.search(j).group())
+            #print(c)
 
-        mongo[db_name][collection_name].insert_one({'IP' : str(i) , 'Country Code' : str(p_ccode.match(j).group()) ,
-                                                  'Connection Count': str(p_ccount.search(j).group()) ,
-                                                   'File_Hash' : h,
+        mongo[db_name][collection_name].insert_one({'IP' : str(i) , 'Country Code' : str(p_ccode.match(j).group())[15:] ,
+                                                  'Connection Count': str(p_ccount.search(j).group())[18:] ,
+                                                   'File_Hash' : h[7:],
                                                   'Command' : c}).inserted_id
-
+    print("DB END")
 
 if __name__=='__main__':
 
@@ -130,8 +132,8 @@ if __name__=='__main__':
     date = ''   # data time_stamp
     attack_command_dic = {}
     temp_downIP = []
-    start_timedate = '2020-3-16'
-
+    start_timedate = '2020-03-24'
+    scp_dic = {}
     print("-------------- START ---------------- ")
 
     #with open(path + date + '.json') as json_file:
@@ -161,22 +163,32 @@ if __name__=='__main__':
                 total_ip.append(attack_ip)
 
             elif(event_id == "cowrie.session.file_download" or event_id == "cowrie.session.file_upload"):
-                file_route = log_data["outfile"]
-                file_hash = file_route[25:]
-                overlap = False
+                realm_check = json_key_present(log_data, 'realm')
+                if realm_check:
+                    scp = log_data['input']
 
-                if (file_hash in hash_dic.values()) == False:
-                    for hash_i in range(len(hash_dic.keys())+1):
-                        if(hash_dic.get(attack_ip) == None):
-                            overlap = True
-                        else:
-                            overlap = False
-
-
-                    if (overlap):
-                        hash_dic[attack_ip] = file_hash
+                    if attack_ip in scp_dic:
+                        scp_dic[attack_ip].append(scp)
                     else:
-                        hash_dic[attack_ip] = (hash_dic[attack_ip] + ', ' +file_hash)
+                        scp_dic[attack_ip] = [scp]
+
+                else:
+                     file_route = log_data["outfile"]
+                     file_hash = file_route[25:]
+                     overlap = False
+
+                     if (file_hash in hash_dic.values()) == False:
+                        for hash_i in range(len(hash_dic.keys())+1):
+                            if(hash_dic.get(attack_ip) == None):
+                                overlap = True
+                            else:
+                                overlap = False
+
+
+                        if (overlap):
+                            hash_dic[attack_ip] = file_hash
+                        else:
+                             hash_dic[attack_ip] = (hash_dic[attack_ip] + ', ' +file_hash)
 
 
             elif(event_id == "cowrie.command.input"):
@@ -187,33 +199,17 @@ if __name__=='__main__':
                 else:
                     attack_command_dic[attack_ip].append(log_data["input"])
 
+#            elif(event_id == "timestamp"):
+
+
             count += 1
 
     cn_data = cCode(date)
-    #print("-------------------count ip connection------------------")
-    #print(count_ip())
-    #(cn_data.keys())
+
     res_count = 0
 
     temp_countip = count_ip()
 
-    #print(temp_countip)
-
-    # for i, j in cn_data.items(): // 원본
-    #     for k, m in hash_dic.items():
-    #         if k == i:
-    #             cn_data[i] = str(j) + ', Hash : '+str(m)
-    #
-    #     for z in temp_countip.keys():
-    #         if z == i:
-    #             cn_data[i] = str(temp_value) + ', Connection Count : ' + str(cn_data[z])
-    #
-    #     for q, w in attack_command_dic.items():
-    #         if q == i:
-    #             temp_value = list(cn_data.values())
-    #             cn_data[i] = str(temp_value[res_count]) + ', Command : ' + str(w)
-    #
-    #     res_count += 1
 
     for i in cn_data.keys():
         temp = list(cn_data.values())
@@ -240,21 +236,6 @@ if __name__=='__main__':
 
     insert_DB(cn_data)
 
-    # print("------------------ Country code & Hash data append ---------------------")
-    # print(cn_data)
-    # print("------------------------------------------------------------------------")
-
     del hash_dic
     del attack_command_dic
     del temp_downIP
-
-    # ip_graph = cCode(date)
-    # print(ip_graph.items())
-    # # x축, y축, 넓이, 색
-    # plt.bar(  list(ip_graph.keys()), list(ip_graph.values()), width = 0.9, color = "blue")
-    # plt.title("Connect request information")
-    # plt.xlabel('Country Code')
-    # plt.ylabel('Count')
-    # plt.xticks(list(ip_graph.keys()), list(ip_graph.keys()), fontsize = 10, rotation = 30)
-    # plt.show()
-    # print(ip_graph.items())
